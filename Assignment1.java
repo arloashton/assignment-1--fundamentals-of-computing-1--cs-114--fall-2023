47fe810fec57063c18b546d9a4bb023145461827
arloashton <143569631+arloashton@users.noreply.github.com>
2023-09-20 09:01:51 -0400

    wrote the code
    wrote the code

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,30 @@
 // Import libraries here
-
+import java.util.Scanner;
+// Arlo Ashton
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
+    String userString;
+    float userTemperature;
+
+    //PRINTS INITIALS
+    System.out.println(
+     "\n      A             A       "
+    +"\n     A A           A A      "
+    +"\n    A   A         A   A     "
+    +"\n   AAAAAAA       AAAAAAA    "
+    +"\n  AA     AA     AA     AA   "
+    +"\n AAA     AAA   AAA     AAA  "
+    );
+    //GETS USER VALUES FOR STRING AND TEMPERATURE
+Scanner stringScan = new Scanner(System.in);
+System.out.println("Enter a 5 Character Long String:");
+userString = stringScan.nextLine();
+
+Scanner temperatureScan = new Scanner(System.in);
+System.out.println("Enter a temperature value in farenheit:");
+userTemperature = temperatureScan.nextFloat();
+
+float celcius = (userTemperature - 32) * 5/9;
+System.out.println(celcius);
   }
 }
